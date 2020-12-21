62c4e4bbfba029b7c368659638d0833a91bb2087
ZainaSaadeddin <64072872+ZainaSaadeddin@users.noreply.github.com>
2020-12-21 14:36:38 +0200

    Add files via upload
    I separated the cli.ts file into 3 files:
    index.ts, functions.ts, and helper.ts
    
    index.ts file contains commands.
    functions.ts contains async functions.
    helper.ts file contains sync functions

---
+++ src/functions.ts
@@ -0,0 +1,319 @@
+import axios from 'axios';
+import * as fs from "fs";
+import opn from 'opn';
+import {
+  dumpJSON,
+  in_es6,
+  loadConfig,
+  parseId,
+  parseIds,
+  showDepositionJSON,
+  updateMetadata
+} from "./helper";
+
+async function publishDeposition(args, id) {
+  id = parseId(id);
+  const {zenodoAPIUrl, params} = loadConfig(args.config);
+  const res = await axios.post(`${zenodoAPIUrl}/${id}/actions/publish`, {"params": params});
+  if ((res.data !== 202)) {
+    console.log(`Error in publshing deposition ${id}: `, res.data);
+  } else {
+    console.log(`\tDeposition ${id} successfully published.`);
+  }
+}
+
+async function showDeposition(args, id) {
+  const info = await getData(args, parseId(id));
+  showDepositionJSON(info);
+}
+
+async function getData(args, id) {
+  const {zenodoAPIUrl, params} = loadConfig(args.config);
+  id = parseId(id);
+  let res = await axios.get(`${zenodoAPIUrl}/${id}`, {"params": params});
+  if ((res.status !== 200)) {
+    if ((res.status["status"] !== 404)) {
+      console.log(`Error in getting data: ${res.data}`);
+      process.exit(1);
+    } else {
+      console.log("Checking helper ID.");
+      const listParams = params;
+      listParams["q"] = ("conceptrecid:" + id);
+      res = await axios.get(zenodoAPIUrl, {"params": listParams});
+      if ((res.status !== 200)) {
+        console.log(`Failed in getting data: ${res.data}`);
+      } else {
+        console.log(("Found record ID: " + res.data[0]["id"].toString()));
+        return res.data[0];
+      }
+    }
+  } else {
+    return res.data;
+  }
+}
+
+async function getMetadata(args, id) {
+  return getData(args, id)["metadata"];
+}
+
+async function createRecord(args, metadata) {
+  console.log("\tCreating record.");
+  const {zenodoAPIUrl, params} = loadConfig(args.config);
+  const res = await axios.post(zenodoAPIUrl, {"json": {"metadata": metadata}, "params": params});
+  if ((res.status !== 201)) {
+    console.log(`Error in creating new record: ${res.data}`);
+    process.exit(1);
+  }
+  return res.data;
+}
+
+async function editDeposit(args, dep_id) {
+  const {zenodoAPIUrl, params} = loadConfig(args.config);
+  const res = await axios.post(`${zenodoAPIUrl}/${parseId(dep_id)}/actions/edit`, {"params": params});
+  if ((res.status !== 201)) {
+    console.log(`Error in making record editable. ${res.data}`);
+    process.exit(1);
+  }
+  return res.data;
+}
+
+async function updateRecord(args, dep_id, metadata) {
+  console.log("\tUpdating record.");
+  const {zenodoAPIUrl, params} = loadConfig(args.config);
+  const res = await axios.put(`${zenodoAPIUrl}/${parseId(dep_id)}`, {"json": {"metadata": metadata}, "params": params});
+  if ((res.status !== 200)) {
+    console.log(`Error in updating record. ${res.data}`);
+    process.exit(1);
+  }
+  return res.data;
+}
+
+async function fileUpload(args, bucket_url, journal_filepath) {
+  const {params} = loadConfig(args.config);
+  console.log("\tUploading file.");
+  const replaced = journal_filepath.replace("^.*\\/", "");
+  const data = fs.readFileSync(journal_filepath, "utf8");
+  const res = await axios.put(((bucket_url + "/") + replaced), {"data": data, "params": params});
+  if ((res.status !== 200)) {
+    process.exit(res.data);
+  }
+  console.log("\tUpload successful.");
+}
+
+async function finalActions(args, id, deposit_url) {
+  if ((in_es6("publish", args) && args.publish)) {
+    await publishDeposition(args, id);
+  }
+  if ((in_es6("show", args) && args.show)) {
+    await showDeposition(args, id);
+  }
+  if ((in_es6("dump", args) && args.dump)) {
+    await dumpDeposition(args, id);
+  }
+  if ((in_es6("open", args) && args.open)) {
+    //webbrowser.open_new_tab(deposit_url);
+    opn(deposit_url);
+  }
+}
+
+export async function saveIdsToJson(args) {
+  let data, f, ids;
+  ids = parseIds(args.id);
+  for (let id, _pj_c = 0, _pj_a = ids, _pj_b = _pj_a.length; (_pj_c < _pj_b); _pj_c += 1) {
+    id = _pj_a[_pj_c];
+    data = await getData(args, id);
+    f = fs.writeFileSync(`${id}.json`, data["metadata"], {encoding: 'utf8'});
+    f.close();
+    await finalActions(args, id, data["links"]["html"]);
+  }
+}
+
+export async function dumpDeposition(args, id) {
+  const info = await getData(args, parseId(id));
+  dumpJSON(info);
+}
+
+export async function duplicate(args) {
+  var bucket_url, deposit_url, metadata, response_data;
+  metadata = getMetadata(args, args.id[0]);
+  delete metadata["doi"];
+  metadata["prereserve_doi"] = true;
+  metadata = updateMetadata(args, metadata);
+  response_data = createRecord(args, metadata);
+  bucket_url = response_data["links"]["bucket"];
+  deposit_url = response_data["links"]["html"];
+  if (args.files) {
+    for (var filePath, _pj_c = 0, _pj_a = args.files, _pj_b = _pj_a.length; (_pj_c < _pj_b); _pj_c += 1) {
+      filePath = _pj_a[_pj_c];
+      await fileUpload(args, bucket_url, filePath);
+    }
+  }
+  await finalActions(args, response_data["id"], deposit_url);
+}
+
+export async function upload(args) {
+  var bucket_url, deposit_url, response;
+  bucket_url = null;
+  if (args.bucketurl) {
+    bucket_url = args.bucketurl;
+  } else {
+    if (args.id) {
+      response = getData(args, args.id);
+      bucket_url = response["links"]["bucket"];
+      deposit_url = response["links"]["html"];
+    }
+  }
+  if (bucket_url) {
+    for (var filePath, _pj_c = 0, _pj_a = args.files, _pj_b = _pj_a.length; (_pj_c < _pj_b); _pj_c += 1) {
+      filePath = _pj_a[_pj_c];
+      await fileUpload(args, bucket_url, filePath);
+    }
+    await finalActions(args, args.id, deposit_url);
+  } else {
+    console.log("Unable to upload: id and bucketurl both not specified.");
+  }
+}
+
+export async function update(args) {
+  var bucket_url, data, deposit_url, id, metadata, response_data;
+  id = parseId(args.id[0]);
+  data = getData(args, id);
+  metadata = data["metadata"];
+  if ((data["state"] === "done")) {
+    console.log("\tMaking record editable.");
+    response_data = editDeposit(args, id);
+  } else {
+    metadata = updateMetadata(args, metadata);
+    response_data = updateRecord(args, id, metadata);
+  }
+  bucket_url = response_data["links"]["bucket"];
+  deposit_url = response_data["links"]["html"];
+  if (args.files) {
+    for (var filePath, _pj_c = 0, _pj_a = args.files, _pj_b = _pj_a.length; (_pj_c < _pj_b); _pj_c += 1) {
+      filePath = _pj_a[_pj_c];
+      await fileUpload(args, bucket_url, filePath);
+    }
+  }
+  await finalActions(args, id, deposit_url);
+}
+
+export async function copy(args) {
+  var bucket_url, metadata, response_data;
+  metadata = getMetadata(args, args.id);
+  delete metadata["doi"];
+  delete metadata["prereserve_doi"];
+  for (var journal_filepath, _pj_c = 0, _pj_a = args.files, _pj_b = _pj_a.length; (_pj_c < _pj_b); _pj_c += 1) {
+    journal_filepath = _pj_a[_pj_c];
+    console.log(("Processing: " + journal_filepath));
+    response_data = await createRecord(args, metadata);
+    bucket_url = response_data["links"]["bucket"];
+    await fileUpload(args, bucket_url, journal_filepath);
+    await finalActions(args, response_data["id"], response_data["links"]["html"]);
+  }
+}
+
+export async function listDepositions(args) {
+  const {zenodoAPIUrl, params} = loadConfig(args.config);
+  params["page"] = args.page;
+  params["size"] = (args.size ? args.size : 1000);
+  const res = await axios.get(zenodoAPIUrl, {params});
+  if ((res.status !== 200)) {
+    console.log(`Failed in listDepositions: ${res.data}`);
+    process.exit(1);
+  }
+  if ((in_es6("dump", args) && args.dump)) {
+    dumpJSON(res.data);
+  }
+  for (var dep, _pj_c = 0, _pj_a = res.data, _pj_b = _pj_a.length; (_pj_c < _pj_b); _pj_c += 1) {
+    dep = _pj_a[_pj_c];
+    console.log(dep["record_id"], dep["conceptrecid"]);
+    if ((in_es6("publish", args) && args.publish)) {
+      await publishDeposition(args, dep["id"]);
+    }
+    if ((in_es6("show", args) && args.show)) {
+      showDepositionJSON(dep);
+    }
+    if ((in_es6("open", args) && args.open)) {
+      opn(dep["links"]["html"]);
+    }
+  }
+}
+
+export async function newVersion(args) {
+  const {zenodoAPIUrl, params} = loadConfig(args.config);
+  const id = parseId(args.id[0]);
+  const response = await axios.post(`${zenodoAPIUrl}/${id}/actions/newversion`, {"params": params});
+  if ((response.status !== 201)) {
+    console.log(`New version request failed:`, response.data);
+    process.exit(1);
+  }
+  let response_data = response.data;
+  const metadata = await getMetadata(args, id);
+  const newmetadata = updateMetadata(args, metadata);
+  if ((newmetadata !== metadata)) {
+    response_data = updateRecord(args, id, newmetadata);
+  }
+  const bucket_url = response_data["links"]["bucket"];
+  const deposit_url = response_data["links"]["latest_html"];
+  if (args.files) {
+    for (var filePath, _pj_c = 0, _pj_a = args.files, _pj_b = _pj_a.length; (_pj_c < _pj_b); _pj_c += 1) {
+      filePath = _pj_a[_pj_c];
+      await fileUpload(args, bucket_url, filePath);
+    }
+  }
+  await finalActions(args, response_data["id"], deposit_url);
+  console.log("latest_draft: ", response_data["links"]["latest_draft"]);
+}
+
+export async function download(args) {
+  var data, fp, id, name;
+  id = parseId(args.id[0]);
+  data = await getData(args, id);
+  const {params} = loadConfig(args.config);
+  for (var fileObj, _pj_c = 0, _pj_a = data["files"], _pj_b = _pj_a.length; (_pj_c < _pj_b); _pj_c += 1) {
+    fileObj = _pj_a[_pj_c];
+    name = fileObj["filename"];
+    console.log(`Downloading ${name}`);
+    const res = await axios.get(fileObj["links"]["download"], {"params": params});
+    fp = open(name, "wb+");
+    fp.write(res.data);
+    fp.close();
+    fp = open((name + ".md5"), "w+");
+    fp.write(((fileObj["checksum"] + " ") + fileObj["filename"]));
+    fp.close();
+  }
+}
+
+export async function concept(args) {
+  const {zenodoAPIUrl, params} = loadConfig(args.config);
+  params["q"] = `conceptrecid:${parseId(args.id[0])}`;
+  const res = await axios.get(zenodoAPIUrl, {"params": params});
+  if ((res.status !== 200)) {
+    console.log(`Failed in concept(args): `, res.data);
+    process.exit(1);
+  }
+  if ((in_es6("dump", args) && args.dump)) {
+    dumpJSON(res.data);
+  }
+  for (var dep, _pj_c = 0, _pj_a = res.data, _pj_b = _pj_a.length; (_pj_c < _pj_b); _pj_c += 1) {
+    dep = _pj_a[_pj_c];
+    console.log(dep["record_id"], dep["conceptrecid"]);
+    if ((in_es6("publish", args) && args.publish)) {
+      await publishDeposition(args, dep["id"]);
+    }
+    if ((in_es6("show", args) && args.show)) {
+      showDepositionJSON(dep);
+    }
+    if ((in_es6("open", args) && args.open)) {
+      opn(dep["links"]["html"]);
+    }
+  }
+}
+
+
+export async function create(args) {
+  const f = fs.readFileSync("blank.json", {encoding: 'utf8'});
+  const metadata = updateMetadata(args, JSON.parse(f));
+  const response_data = await createRecord(args, metadata);
+  await finalActions(args, response_data["id"], response_data["links"]["html"]);
+}
---
+++ src/helper.ts
@@ -0,0 +1,199 @@
+import * as fs from "fs";
+
+const FALLBACK_CONFIG_FILE = (process.env.HOME + "/.config/zenodo-cli/config.json");
+
+export function in_es6(left, right) {
+  if (((right instanceof Array) || ((typeof right) === "string"))) {
+    return (right.indexOf(left) > (-1));
+  } else {
+    if (((right instanceof Map) || (right instanceof Set) || (right instanceof WeakMap) || (right instanceof WeakSet))) {
+      return right.has(left);
+    } else {
+      return (left in right);
+    }
+  }
+}
+
+export function loadConfig(configFile) {
+  if (fs.statSync(configFile).isFile()) {
+  } else {
+    if (fs.statSync(FALLBACK_CONFIG_FILE).isFile()) {
+      configFile = FALLBACK_CONFIG_FILE;
+    } else {
+      console.log(`Config file not present at config.json or ${FALLBACK_CONFIG_FILE}`);
+      process.exit(1);
+    }
+  }
+
+  const content = fs.readFileSync(configFile, "utf8");
+  const config = JSON.parse(content);
+
+  const params = {"access_token": config["accessToken"]};
+
+  let zenodoAPIUrl = "";
+  if ((config["env"] === "sandbox")) {
+    zenodoAPIUrl = "https://sandbox.zenodo.org/api/deposit/depositions";
+  } else {
+    zenodoAPIUrl = "https://zenodo.org/api/deposit/depositions";
+  }
+  return {params, zenodoAPIUrl}
+}
+
+export function parseId(id) {
+  var dot_split, slash_split;
+  if (id.toString().isnumeric()) {
+    return id;
+  }
+  slash_split = id.toString().split("/").slice((-1))[0];
+  if (slash_split.isnumeric()) {
+    id = slash_split;
+  } else {
+    dot_split = id.toString().split(".").slice((-1))[0];
+    if (dot_split.isnumeric()) {
+      id = dot_split;
+    }
+  }
+  return id;
+}
+
+export function showDepositionJSON(info) {
+  console.log(`Title: ${info["title"]}`);
+  if (in_es6("publication_date", info["metadata"])) {
+    console.log(`Date: ${info["metadata"]["publication_date"]}`);
+  } else {
+    console.log("Date: N/A");
+  }
+  console.log(`RecordId: ${info["id"]}`);
+  if (in_es6("conceptrecid", info.keys())) {
+    console.log(`ConceptId: ${info["conceptrecid"]}`);
+  } else {
+    console.log("ConceptId: N/A");
+  }
+  console.log(`DOI: ${info["metadata"]["prereserve_doi"]["doi"]}`);
+  console.log(`Published: ${info["submitted"] ? "yes" : "no"}`);
+  console.log(`State: ${info["state"]}`);
+  console.log(`URL: https://zenodo.org/${info["submitted"] ? "record" : "deposit"}/${info["id"]}`);
+  if (in_es6("bucket", info["links"].keys())) {
+    console.log(`BucketURL: ${info["links"]["bucket"]}`);
+  } else {
+    console.log("BucketURL: N/A");
+  }
+  console.log("\n");
+}
+
+export function dumpJSON(info) {
+  console.log(info);
+  console.log("\n");
+}
+
+export function parseIds(genericIds) {
+  return function () {
+    var _pj_a = [], _pj_b = genericIds;
+    for (var _pj_c = 0, _pj_d = _pj_b.length; (_pj_c < _pj_d); _pj_c += 1) {
+      var id = _pj_b[_pj_c];
+      _pj_a.push(parseId(id));
+    }
+    return _pj_a;
+  }
+    .call(this);
+}
+
+export function updateMetadata(args, metadata) {
+  var author_data_dict, author_data_fp, author_info, comm, creator, meta_file;
+  author_data_dict = {};
+  if ((in_es6("json", args.__dict__) && args.json)) {
+    meta_file = open(args.json);
+//for (key, value) in json.load(meta_file).items():
+//metadata[key] = value
+    meta_file.close();
+  }
+  if (in_es6("creators", metadata)) {
+
+    var _pj_auth = [], _pj_b = metadata["creators"];
+    for (var _pj_c = 0, _pj_d = _pj_b.length; (_pj_c < _pj_d); _pj_c += 1) {
+      var creator = _pj_b[_pj_c];
+      _pj_auth.push(creator["name"]);
+    }
+
+    metadata["authors"] = _pj_auth.join(";");
+
+  }
+
+  if ((in_es6("title", args.__dict__) && args.title)) {
+    metadata["title"] = args.title;
+  }
+  if ((in_es6("date", args.__dict__) && args.date)) {
+    metadata["publication_date"] = args.date;
+  }
+  if ((in_es6("description", args.__dict__) && args.description)) {
+    metadata["description"] = args.description;
+  }
+  if ((in_es6("add_communites", args.__dict__) && args.add_communites)) {
+
+    var _pj_com = [], _pj_b = args.add_communities;
+    for (var _pj_c = 0, _pj_d = _pj_b.length; (_pj_c < _pj_d); _pj_c += 1) {
+      var community = _pj_b[_pj_c];
+      _pj_com.push({"identifier": community});
+    }
+
+    metadata["communities"] = _pj_com;
+  }
+  if ((in_es6("remove_communities", args.__dict__) && args.remove_communities)) {
+
+    var _pj_rrcom = [], _pj_b = metadata["communities"];
+    for (var _pj_c = 0, _pj_d = _pj_b.length; (_pj_c < _pj_d); _pj_c += 1) {
+      var community = _pj_b[_pj_c];
+
+      if (!in_es6(community, args.remove_communities)) {
+        _pj_rrcom.push({"identifier": community});
+      }
+    }
+
+    metadata["communities"] = _pj_rrcom;
+  }
+  if ((in_es6("communities", args.__dict__) && args.communities)) {
+    comm = open(args.communities);
+    metadata["communities"] = function () {
+      var _pj_a = [], _pj_b = comm.read().splitlines();
+      for (var _pj_c = 0, _pj_d = _pj_b.length; (_pj_c < _pj_d); _pj_c += 1) {
+        var community = _pj_b[_pj_c];
+        _pj_a.push({"identifier": community});
+      }
+      return _pj_a;
+    }
+      .call(this);
+    comm.close();
+  }
+  if ((in_es6("authordata", args.__dict__) && args.authordata)) {
+    author_data_fp = open(args.authordata);
+    for (var author_data, _pj_c = 0, _pj_a = author_data_fp.read().splitlines(), _pj_b = _pj_a.length; (_pj_c < _pj_b); _pj_c += 1) {
+      author_data = _pj_a[_pj_c];
+      if (author_data.strip()) {
+        creator = author_data.split("\t");
+        author_data_dict["name"] = {"name": creator[0], "affiliation": creator[1], "orcid": creator[2]};
+      }
+    }
+    author_data_fp.close();
+  }
+  if ((in_es6("authors", args.__dict__) && args.authors)) {
+    metadata["creators"] = [];
+    for (var author, _pj_c = 0, _pj_a = args.authors.split(";"), _pj_b = _pj_a.length; (_pj_c < _pj_b); _pj_c += 1) {
+      author = _pj_a[_pj_c];
+      author_info = author_data_dict.get(author, null);
+      metadata["creators"].append({
+        "name": author,
+        "affiliation": (author_info ? author_info["affiliation"] : ""),
+        "orcid": (author_info ? author_info["orcid"] : "")
+      });
+    }
+  }
+  if ((in_es6("zotero_link", args.__dict__) && args.zotero_link)) {
+    metadata["related_identifiers"] = [{
+      "identifier": args.zotero_link,
+      "relation": "isAlternateIdentifier",
+      "resource_type": "other",
+      "scheme": "url"
+    }];
+  }
+  return metadata;
+}
